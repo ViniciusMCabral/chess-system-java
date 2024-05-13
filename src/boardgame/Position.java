772fd5e42242883b4f12dcbf3e89fcdeb89c58b2
Vinicius Cabral <vmachakcabral@gmail.com>
2024-05-13 18:02:58 -0300

    Implemanting possible moves of Rook

--- src/boardgame/Position.java
+++ src/boardgame/Position.java
@@ -18,14 +18,19 @@ public class Position {
 		this.row = row;
 	}
 
-	public int getColum() {
+	public int getColumn() {
 		return column;
 	}
 
-	public void setColum(int colum) {
-		this.column = colum;
+	public void setColumn(int column) {
+		this.column = column;
 	}
-
+	
+	public void setValues(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+	
 	@Override
 	public String toString() {
 		return row + ", " + column;
